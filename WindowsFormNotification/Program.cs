c1e5c8f202de2ac6da8280764ecae3dee02cf66a
Abanob-Ashraf <abanobashraf74@gmail.com>
2025-04-07 20:27:11 +0200

    update program cs

--- WindowsFormNotification/Program.cs
+++ WindowsFormNotification/Program.cs
@@ -18,25 +18,8 @@ namespace WindowsFormNotification
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new NotificationForm());
 
-            //// Your form (hidden)
-            //var form = new NotificationForm();
-            //form.Load += (s, e) => form.Hide(); // Hide on load
-
-            //// Start app loop with ApplicationContext
-            //Application.Run(new HiddenAppContext(form));
             var context = new NotificationAppContext();
             Application.Run(context);
         }
     }
-
-    //public class HiddenAppContext : ApplicationContext
-    //{
-    //    private Form _form;
-
-    //    public HiddenAppContext(Form form)
-    //    {
-    //        _form = form;
-    //        _form.FormClosed += (s, e) => ExitThread(); // Auto-close app when form closes (if you ever show it)
-    //    }
-    //}
 }
